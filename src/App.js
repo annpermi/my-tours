ac011f404f19ba736e99e933abff2e6d63288fb4
annpermi <perminova.anna85@gmail.com>
2021-06-11 17:44:38 -0400

    Fetched data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tour'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours)
+    } catch (error){
+      setLoading(false);
+      console.log('error')
+    }
+  }
+
+  useEffect(() => {
+    fetchTours()
+  },[])
+
+  if(loading){
+    return <main>
+              <Loading />
+            </main>
+  }
+  return <main>
+    <Tours />
+  </main>
 }
 
 export default App;
